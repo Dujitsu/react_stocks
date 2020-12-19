df643ebec6f14b08d406fa01eff435e33497ef60
Trevor Smith <trevorsmith86@hotmail.com>
2020-12-18 23:03:15 -0600

    added folders to src and rearranged files

--- src/components/test.js
+++ src/components/test.js
@@ -1,1 +1,28 @@
-//TODO
+import React from 'react';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class App extends React.Component {
+    state = { advice: '' };
+    
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const {advice} = response.data.slip;
+                this.setState({ advice })
+            })
+            .catch((error => {
+                console.log(error);
+            }))
+    }    
+    render() { 
+        const { advice } = this.state;
+        return ( <h1>{ advice }</h1> );
+    }
+}
+export default App;
